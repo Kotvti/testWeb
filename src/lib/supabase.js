25af571c4b9da8ceb56dcf39e6ee1e1c5ae5ed68
KotVti <gggke@mail.ru>
2025-02-04 23:16:56 +0100

    DB connect added ver 6

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,13 +1,13 @@
 import { createClient } from '@supabase/supabase-js';
 
-    console.log('Supabase URL:', process.env.NEXT_PUBLIC_SUPABASE_URL);
-    console.log('Supabase Key:', process.env.NEXT_PUBLIC_SUPABASE_KEY);
-    console.log('Supabase URL2:', process.env.DB_SUPABASE_URL);
-    console.log('Supabase Key2:', process.env.DB_SUPABASE_KEY);
+    console.log('Supabase URL:', process.env.REACT_APP_API_URL);
+    console.log('Supabase Key:', process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+    console.log('Supabase URL2:', process.env.REACT_APP_DB_SUPABASE_URL);
+    console.log('Supabase Key2:', process.env.REACT_APP_DB_SUPABASE_KEY);
     console.log('TTTTT!!!!!TTTT', '123');
     console.log('TTTTT!!!!!TTTT');
 
-const supabaseUrl = process.env.DB_SUPABASE_URL;
-const supabaseKey = process.env.DB_SUPABASE_KEY;
+const supabaseUrl = process.env.REACT_APP_DB_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_DB_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
